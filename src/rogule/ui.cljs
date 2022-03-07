d4e6a1fa04651950b5c5034b2568491e7b38b0b2
Chris McCormick <chris@mccormick.cx>
2022-03-07 21:17:45 +0800

    Hacked in movement.

--- src/rogule/ui.cljs
+++ src/rogule/ui.cljs
@@ -20,33 +20,44 @@
               (:_y1 room))
            2))])
 
+(defn process-game-key [state ev]
+  (js/console.log (aget ev "keyCode"))
+  (case (aget ev "keyCode")
+    ;27 (swap! state assoc :screen :menu)
+    37 (swap! state update-in [:entities :air 0 :pos 0] dec)
+    39 (swap! state update-in [:entities :air 0 :pos 0] inc)
+    38 (swap! state update-in [:entities :air 0 :pos 1] dec)
+    40 (swap! state update-in [:entities :air 0 :pos 1] inc)
+    nil))
+
 (defn component-main [_state]
   (let [tiles (-> @state :map :tiles)
         entities (entities-by-pos (-> @state :entities :air))]
-  [:div#game
-   (for [y (range 32)]
+  [:div#game {:on-key-down #(process-game-key state %)}
+   (for [y (range 20)]
      [:div.row
-      (for [x (range 32)]
+      (for [x (range 20)]
         [:span.grid #_ {:style {:background-color "#f00"}}
          (when (= (get tiles [x y]) 0)
-           (tile "1F7EB" "")
-           ;(tile "2B1C" "")
-           )
+           (tile "1F7EB" ""))
          (let [entity (get entities [x y])]
            (when entity
              (tile (:char entity) "")))])])]))
 
 (defn start {:dev/after-load true} []
-  (let [m (make-digger-map (js/Math.random) 32 32)
+  (let [m (make-digger-map (js/Math.random) 20 20)
         first-room (first (:rooms m))
         first-room-center (room-center first-room)]
     (log "map" m)
     (swap! state assoc
            :map m
            :entities {:floor [] ; floor layer
-                      :air [{:char "1F9DD" :pos first-room-center}]}))
+                      :air [{:char "1F9DD"
+                             :pos first-room-center
+                             :id :player}]}))
   (rdom/render [component-main state]
                (js/document.getElementById "app")))
 
 (defn main! []
+  (.addEventListener js/window "keydown" #(process-game-key state %))
   (start))
