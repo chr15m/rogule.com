54ed1003567164c793ccfd81330e50a0a5d0814c
Chris McCormick <chris@mccormick.cx>
2022-03-13 08:00:40 +0800

    Different activation distances for monsters.

--- src/rogule/ui.cljs
+++ src/rogule/ui.cljs
@@ -94,39 +94,50 @@
 
 (def monster-table
   [{:sprite (load-sprite :rat)
+    :activation 3
     :stats {:xp 1 :hp [2 2]}
     :name "the rat"}
    {:sprite (load-sprite :bat)
+    :activation 10
     :stats {:xp 1 :hp [3 3]}
     :name "the bat"}
 
    {:sprite (load-sprite :ghost)
+    :activation 10
     :stats {:xp 2 :hp [3 3]}
     :name "the ghost"}
    {:sprite (load-sprite :boar)
+    :activation 15
     :stats {:xp 2 :hp [4 4]}
     :name "the boar"}
    {:sprite (load-sprite :wolf)
+    :activation 20
     :stats {:xp 2 :hp [5 5]}
     :name "the wolf"}
    {:sprite (load-sprite :ogre)
+    :activation 10
     :stats {:xp 2 :hp [7 7]}
     :name "the ogre"}
 
    {:sprite (load-sprite :vampire)
+    :activation 15
     :stats {:xp 3 :hp [8 8]}
     :name "the vampire"}
    {:sprite (load-sprite :zombie)
+    :activation 5
     :stats {:xp 3 :hp [9 9]}
     :name "the zombie"}
    {:sprite (load-sprite :genie)
+    :activation 20
     :stats {:xp 3 :hp [10 10]}
     :name "the genie"}
 
    {:sprite (load-sprite :dragon)
+    :activation 10
     :stats {:xp 4 :hp [15 15]}
     :name "the dragon"}
    {:sprite (load-sprite :t-rex)
+    :activation 15
     :stats {:xp 4 :hp [12 12]}
     :name "the t-rex"}])
 
@@ -270,7 +281,7 @@
                            (:pos monster) (:pos player)
                            passable-fn))]
     (if (and player
-             (< (count path-to-player) 10)
+             (< (count path-to-player) (:activation monster))
              ; every now and then just don't move
              (< (.getUniform combat-dice) 0.9))
       (move-to *state monster-id (second path-to-player))
