76169407ab846b9c75c75e3850fcd88b14669469
Chris McCormick <chris@mccormick.cx>
2022-06-01 11:33:03 +0800

    Added rate limited anonymous game record endpoint.

--- src/rogule/server.cljs
+++ src/rogule/server.cljs
@@ -3,33 +3,45 @@
     ["fs" :as fs]
     [applied-science.js-interop :as j]
     [promesa.core :as p]
-    [sitefox.html :refer [render-into]]
     [sitefox.web :as web]
     [sitefox.util :refer [env]]
-    [sitefox.tracebacks :refer [install-traceback-emailer]]))
-
+    [sitefox.tracebacks :refer [install-traceback-emailer]]
+    [sitefox.db :refer [kv]]
+    [sitefox.deps :refer [body-parser csrf]]
+    ["express-slow-down" :as slow]))
 
 (let [admin-email (env "ADMIN_EMAIL")]
   (when admin-email
     (install-traceback-emailer admin-email)))
 
+(def rate-limiter (slow #js {:windowMs (* 1000 60 5)
+                             :delayAfter 60
+                             :delayMs 200}))
+
 (defonce server (atom nil))
 
 (def template (fs/readFileSync "public/index.html"))
 
-(defn my-page []
-  [:main
-   [:h1 "My page"]
-   [:p "This is a server rendered page."]
-   [:p [:a {:href "/"} "Return to the app"]]])
-
-(defn api-example [_req res]
-  (.json res (clj->js {:question 42})))
+(defn store-game-record [req res]
+  (p/let [_games (kv "game-records")
+          game-data (j/get req :body)
+          date (-> (js/Date.) .toISOString (.split "T") first)
+          id (-> (random-uuid) .toString (.split "-") first)
+          k (str date ":" id)
+          size (count (js/JSON.stringify game-data))
+          size-limit 20000]
+    ;(.set games k game-data)
+    (js/console.log k game-data size)
+    (if (> size size-limit)
+      (-> res
+          (.status 403)
+          (.send "Forbidden"))
+      (.json res game-data))))
 
 (defn setup-routes [app]
   (web/reset-routes app)
-  (j/call app :get "/mypage" #(.send %2 (render-into template "body" [my-page])))
-  (j/call app :get "/api/example.json" api-example)
+  (j/call app :post "/share" store-game-record)
+  (j/call app :use rate-limiter)
   (web/static-folder app "/twemoji" "node_modules/twemoji-emojis/vendor")
   (web/static-folder app "/" "public"))
 
